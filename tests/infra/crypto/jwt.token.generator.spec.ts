3213af0cce1a7cb2a0ebc442302463bb64bba427
Christiangsn <christianguimaraes1996@gmail.com>
2022-07-17 15:04:48 -0300

    isolate SUT creation

--- tests/infra/crypto/jwt.token.generator.spec.ts
+++ tests/infra/crypto/jwt.token.generator.spec.ts
@@ -17,10 +17,18 @@ class JwtTokenGenerator {
 }
 
 describe('JwtTokenGenerator', () => {
-  it('Should call sign with correct params', async () => {
-    const fakeJwt = jwt as jest.Mocked<typeof jwt>
-    const sut = new JwtTokenGenerator('any_secret')
+  let sut: JwtTokenGenerator
+  let fakeJwt: jest.Mocked<typeof jwt>
 
+  beforeAll(() => {
+    fakeJwt = jwt as jest.Mocked<typeof jwt>
+  })
+
+  beforeEach(() => {
+    sut = new JwtTokenGenerator('any_secret')
+  })
+
+  it('Should call sign with correct params', async () => {
     await sut.generation({ key: 'any_key', expirationInMs: 1000 })
     expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, 'any_secret', { expiresIn: 1 })
   })
