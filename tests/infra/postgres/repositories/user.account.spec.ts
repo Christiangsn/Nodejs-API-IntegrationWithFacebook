c2edb627c4f2c5e74f18f295ce20d4f9c7f63c76
Christiangsn <christianguimaraes1996@gmail.com>
2022-07-22 11:12:45 -0300

    share SUT creation

--- tests/infra/postgres/repositories/user.account.spec.ts
+++ tests/infra/postgres/repositories/user.account.spec.ts
@@ -7,30 +7,29 @@ import { getRepository, Repository, getConnection } from 'typeorm'
 import { makeFakeDb } from '../mocks'
 
 describe('UserAccountRepository', () => {
+  let sut: UserAccountRepository
+  let userRepository: Repository<UserEntity>
+  let backup: IBackup
+
+  beforeAll(async () => {
+    const db = await makeFakeDb([UserEntity])
+    backup = db.backup() // Trás um ponto de restauração, já que fica armazenada a criação de um usuario
+    userRepository = getRepository(UserEntity)
+  })
+
+  afterAll(async () => {
+    await getConnection().close() // TYPEORM desconecta a conexão com o banco ativa
+  })
+
+  beforeEach(() => {
+    backup.restore()
+    sut = new UserAccountRepository()
+  })
+
   describe('load', () => {
-    let sut: UserAccountRepository
-    let userRepository: Repository<UserEntity>
-    let backup: IBackup
-
-    beforeAll(async () => {
-      const db = await makeFakeDb([UserEntity])
-      backup = db.backup() // Trás um ponto de restauração, já que fica armazenada a criação de um usuario
-      userRepository = getRepository(UserEntity)
-    })
-
-    afterAll(async () => {
-      await getConnection().close() // TYPEORM desconecta a conexão com o banco ativa
-    })
-
-    beforeEach(() => {
-      backup.restore()
-      sut = new UserAccountRepository()
-    })
-
-    // TO:DO o db.backup ajudou no empilhamento das chamadas.. ajudando assim a limpar o banco assim que o proximo tester for feito
+    // O db.backup ajudou no empilhamento das chamadas.. ajudando assim a limpar o banco assim que o proximo tester for feito
     it('Should return an account if email exists', async () => {
       await userRepository.save({ email: 'any_email' })
-      const sut = new UserAccountRepository()
 
       const account = await sut.check({ email: 'any_email' })
 
@@ -41,6 +40,17 @@ describe('UserAccountRepository', () => {
       const account = await sut.check({ email: 'any_email' })
 
       expect(account).toBeUndefined()
+    })
+  })
+
+  describe('saveWithFacebook', () => {
+    // O db.backup ajudou no empilhamento das chamadas.. ajudando assim a limpar o banco assim que o proximo tester for feito
+    it('Should return an account if email exists', async () => {
+      await userRepository.save({ email: 'any_email' })
+
+      const account = await sut.check({ email: 'any_email' })
+
+      expect(account).toEqual({ id: '1' })
     })
   })
 })
