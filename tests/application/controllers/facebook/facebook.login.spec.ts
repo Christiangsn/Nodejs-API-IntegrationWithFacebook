731db8217ce5a0b54c08eb82e16924366b11a922
Christiangsn <christianguimaraes1996@gmail.com>
2022-07-28 21:42:07 -0300

    ensure FacebookLoginController calls FacebookAuth with correct params

--- tests/application/controllers/facebook/facebook.login.spec.ts
+++ tests/application/controllers/facebook/facebook.login.spec.ts
@@ -1,8 +1,15 @@
+import { IFacebookAuth } from '@domain/contracts'
+import { mock } from 'jest-mock-extended'
 
 type IHttpResponse = { statusCode: number, data: any}
 
 class FacebookLoginController {
+  constructor (
+    private readonly facebookAuthenticationUseCases: IFacebookAuth
+  ) {}
+
   async run (httpRequest: any): Promise<IHttpResponse> {
+    await this.facebookAuthenticationUseCases.execute({ token: httpRequest.token })
     return {
       statusCode: 400,
       data: new Error('The field token is required')
@@ -13,7 +20,9 @@ class FacebookLoginController {
 describe('FacebookLoginController', () => {
   // Se o token do facebook for vazio
   it('should return 400 if token is empty', async () => {
-    const sut = new FacebookLoginController()
+    // Mockar a interface do caso de uso
+    const FacebookAuthenticationUseCases = mock<IFacebookAuth>()
+    const sut = new FacebookLoginController(FacebookAuthenticationUseCases)
 
     const logon = await sut.run({ token: '' })
     expect(logon).toEqual({
@@ -24,8 +33,9 @@ describe('FacebookLoginController', () => {
 
   // Se o token do facebook for null
   it('should return 400 if token is null', async () => {
-    const sut = new FacebookLoginController()
-
+    // Mockar a interface do caso de uso
+    const FacebookAuthenticationUseCases = mock<IFacebookAuth>()
+    const sut = new FacebookLoginController(FacebookAuthenticationUseCases)
     const logon = await sut.run({ token: null })
     expect(logon).toEqual({
       statusCode: 400,
@@ -35,12 +45,25 @@ describe('FacebookLoginController', () => {
 
   // Se o token do facebook for undefined
   it('should return 400 if token is undefined', async () => {
-    const sut = new FacebookLoginController()
+    // Mockar a interface do caso de uso
+    const FacebookAuthenticationUseCases = mock<IFacebookAuth>()
+    const sut = new FacebookLoginController(FacebookAuthenticationUseCases)
 
     const logon = await sut.run({ token: undefined })
     expect(logon).toEqual({
       statusCode: 400,
       data: new Error('The field token is required')
     })
+  })
+
+  // Chamar o caso de uso do facebook authentication
+  it('should call FacebookAuthenticationUseCases with correct params', async () => {
+    // Mockar a interface do caso de uso
+    const FacebookAuthenticationUseCases = mock<IFacebookAuth>()
+    const sut = new FacebookLoginController(FacebookAuthenticationUseCases)
+
+    await sut.run({ token: 'any_token' })
+    expect(FacebookAuthenticationUseCases.execute).toHaveBeenCalledWith({ token: 'any_token' })
+    expect(FacebookAuthenticationUseCases.execute).toHaveBeenCalledTimes(1)
   })
 })
