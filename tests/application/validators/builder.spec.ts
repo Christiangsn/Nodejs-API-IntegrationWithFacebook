c219c8647be4af523f6ab01dd510044b25b10173
Christiangsn <christianguimaraes1996@gmail.com>
2022-07-30 19:28:47 -0300

    move ValidationBuilder to its own file

--- tests/application/validators/builder.spec.ts
+++ tests/application/validators/builder.spec.ts
@@ -1,25 +1,4 @@
-import { RequiredStringValidator, Validator } from '@app/validators'
-
-class ValidationBuilder {
-  private constructor (
-    private readonly value: string,
-    private readonly fieldName: string,
-    private readonly validators: Validator[] = []
-  ) { }
-
-  public static of (params: { value: string, fieldName: string}): ValidationBuilder {
-    return new ValidationBuilder(params.value, params.fieldName)
-  }
-
-  public required (): ValidationBuilder {
-    this.validators.push(new RequiredStringValidator(this.value, this.fieldName))
-    return this
-  }
-
-  public build (): Validator[] {
-    return this.validators
-  }
-}
+import { RequiredStringValidator, ValidationBuilder } from '@app/validators'
 
 describe('ValidationBuilder', () => {
   it('Should return a RequiredStringValidator', () => {
