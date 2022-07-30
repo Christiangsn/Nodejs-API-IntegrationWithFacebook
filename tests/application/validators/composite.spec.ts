5ec277289945a3bdc74404a881886e3c714ffd18
Christiangsn <christianguimaraes1996@gmail.com>
2022-07-30 14:08:23 -0300

    move production code to their own files

--- tests/application/validators/composite.spec.ts
+++ tests/application/validators/composite.spec.ts
@@ -1,24 +1,6 @@
+import { ValidationComposite, Validator } from '@app/validators'
 import { MockProxy, mock } from 'jest-mock-extended'
 /* eslint-disable @typescript-eslint/no-unused-vars */
-
-interface Validator {
-  validate: () => Error | undefined
-}
-
-class ValidationComposite implements Validator {
-  constructor (
-    private readonly validators: Validator[]
-  ) {}
-
-  public validate (): Error | undefined {
-    for (const validator of this.validators) {
-      const error = validator.validate()
-      if (error !== undefined) {
-        return error
-      }
-    }
-  }
-}
 
 describe('ValidationComposite', () => {
   let sut: ValidationComposite
