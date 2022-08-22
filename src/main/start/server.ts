eeefde568ca159535ec00c24bc5c9d151c956a44
christiangsn <christianguimaraes1996@gmail.com>
2022-08-21 22:29:01 -0300

    move typeorm config to  root folder

--- src/main/start/server.ts
+++ src/main/start/server.ts
@@ -2,9 +2,8 @@
 import { app } from './app'
 import { env } from '../config/env'
 import { createConnection } from 'typeorm'
-import { config } from '../../infra/postgres/connection'
 
-createConnection(config)
+createConnection()
   .then(() => {
     app.listen(env.appPort, () => console.log(`Server running at http://localhost:${env.appPort}`))
   }).catch(console.error)
