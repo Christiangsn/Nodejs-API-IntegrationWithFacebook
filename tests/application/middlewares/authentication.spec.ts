d0ee244d6402e1098de4a5b0abc5900b96d07d52
christiangsn <christianguimaraes1996@gmail.com>
2022-09-04 14:15:08 -0300

    move AuthenticationMIddleware to its own file

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -1,37 +1,7 @@
-import { RequiredStringValidator } from '@app/validators'
 import { ForbiddenError } from '@app/errors/http'
-import { IHttpResponse } from '@app/helpers/http'
-import { Fobidden, Success } from '@app/helpers/responses'
+import { AuthenticationMiddleware } from '@app/middlewares'
 import { IAuthorize } from '@domain/features/auth'
 import { MockProxy, mock } from 'jest-mock-extended'
-
-type IHttpRequest = {
-  authorization: string
-}
-
-type Model = Error | { userId: string }
-
-export class AuthenticationMiddleware {
-  constructor (
-    private readonly authorize: IAuthorize
-  ) { }
-
-  public async handle ({ authorization }: IHttpRequest): Promise<IHttpResponse<Model> > {
-    if (!this.validate({ authorization })) return Fobidden()
-
-    try {
-      const userId = await this.authorize.auth({ token: authorization })
-      return Success({ userId })
-    } catch (error) {
-      return Fobidden()
-    }
-  }
-
-  private validate ({ authorization }: IHttpRequest): boolean {
-    const error = new RequiredStringValidator(authorization, 'authorization').validate()
-    return error === undefined
-  }
-}
 
 describe('AuthenticationMiddleware', () => {
   let sut: AuthenticationMiddleware
