89e6b7165d6144b39668a3df0a5b37bfa6f3b28e
christiangsn <christianguimaraes1996@gmail.com>
2022-09-04 16:14:02 -0300

    ensure auth middleware 200 if authoriztion header is valid

--- tests/main/middlewares/middlewares.auth.spec.ts
+++ tests/main/middlewares/middlewares.auth.spec.ts
@@ -2,7 +2,9 @@
 import { ForbiddenError } from '@app/errors/http'
 import { auth } from '@main/middlewares'
 import { app } from '@main/start/app'
+import { env } from '@main/config/env'
 
+import { sign } from 'jsonwebtoken'
 import request from 'supertest'
 
 describe('authentication Middleware', () => {
@@ -17,14 +19,18 @@ describe('authentication Middleware', () => {
     expect(body.error).toBe(new ForbiddenError().message)
   })
 
-  it('Should return 200 if authorization header was provided', async () => {
+  it('Should return 200 if authorization header is valid', async () => {
+    const authorization = sign({ key: 'any_user_id' }, env.jwtSecret)
     // eslint-disable-next-line @typescript-eslint/promise-function-async
-    app.get('/fakeRoute', (req, res, next) => auth.intercept(req, res, next))
+    app.get('/fakeRoute', (req, res, next) => auth.intercept(req, res, next), (req, res) => {
+      res.json(req.locals)
+    })
 
     const { status, body } = await request(app)
       .get('/fakeRoute')
+      .set({ authorization })
 
-    expect(status).toBe(403)
-    expect(body.error).toBe(new ForbiddenError().message)
+    expect(status).toBe(200)
+    expect(body).toEqual({ userId: 'any_user_id' })
   })
 })
