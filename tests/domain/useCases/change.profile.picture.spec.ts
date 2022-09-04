094ad0a8e52c3cc470c08e0d5b162b9dd3ed3eb8
christiangsn <christianguimaraes1996@gmail.com>
2022-09-04 18:21:17 -0300

    ensure ChangeProfilePicture calls UploadFile with unique id

--- tests/domain/useCases/change.profile.picture.spec.ts
+++ tests/domain/useCases/change.profile.picture.spec.ts
@@ -3,11 +3,17 @@ import { IProfilePicture } from '@domain/features/profile-picture'
 
 export class ChangeProfilePicture implements IProfilePicture {
   constructor (
-    private readonly fileStorage: IUploadFile
+    private readonly fileStorage: IUploadFile,
+    private readonly crypto: IUUIDGenerator
   ) {}
 
   public async save ({ file, id }: IProfilePicture.Input): Promise<IProfilePicture.Output> {
-    await this.fileStorage.upload({ file, key: id })
+    await this.fileStorage.upload({
+      file,
+      key: this.crypto.uuid({
+        key: id
+      })
+    })
   }
 }
 
@@ -22,15 +28,30 @@ namespace IUploadFile {
   }
 }
 
+interface IUUIDGenerator {
+  uuid: (input: IUUIDGenerator.Input) => IUUIDGenerator.Output
+}
+
+namespace IUUIDGenerator {
+  export type Input = {
+    key: string
+  }
+  export type Output = string
+}
+
 describe('ChangeProfilePicture', () => {
   it('Should call UploadFile withj corect input', async () => {
+    const uuid: string = 'any_unique_id'
     const file = Buffer.from('any_buffer')
     const fileStorage = mock<IUploadFile>()
-    const sut = new ChangeProfilePicture(fileStorage)
+    const crypto = mock<IUUIDGenerator>()
+
+    crypto.uuid.mockReturnValue(uuid)
+    const sut = new ChangeProfilePicture(fileStorage, crypto)
 
     await sut.save({ id: 'any_id', file })
 
-    expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: 'any_id' })
+    expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: uuid })
     expect(fileStorage.upload).toHaveBeenCalledTimes(1)
   })
 })
