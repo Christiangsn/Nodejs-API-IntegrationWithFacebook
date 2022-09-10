9bc517cff86007de704fc6c72a8c3b9fc2a40951
christiangsn <christianguimaraes1996@gmail.com>
2022-09-10 01:28:26 -0300

    ensure SavePictureController returns 400 if file is empty

--- tests/application/controllers/save-profile-picture/save-profile-picture.ts
+++ tests/application/controllers/save-profile-picture/save-profile-picture.ts
@@ -15,10 +15,36 @@ export class SavePictureController {
 }
 
 describe('SavePictureController', () => {
-  it('Should return 400 if file is not provided', async () => {
-    const sut = new SavePictureController()
+  let sut: SavePictureController
 
+  beforeEach(() => {
+    sut = new SavePictureController()
+  })
+
+  it('Should return 400 if file is not provided', async () => {
     const httpResponse = await sut.execute({ file: undefined })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('file')
+    })
+  })
+
+  it('Should return 400 if file is null', async () => {
+    const httpResponse = await sut.execute({ file: null })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('file')
+    })
+  })
+
+  it('Should return 400 if file is empty', async () => {
+    const httpResponse = await sut.execute({
+      file: {
+        buffer: Buffer.from('')
+      }
+    })
 
     expect(httpResponse).toEqual({
       statusCode: 400,
